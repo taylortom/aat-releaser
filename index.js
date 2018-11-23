b5fc5caab92bc0da4b4fa1e13d84babd1185249c
Thomas Taylor <thomas.taylor@kineo.com>
2018-11-23 11:27:36 +0000

    Add CLI support

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 const async = require('async');
 const path = require('path');
 const Utils = require('./lib/Utils');
@@ -8,17 +9,15 @@ const package = require('./lib/package');
 
 const DIR = process.cwd();
 
-module.exports = (() => {
-  if(!RegExp(/\d\.\d\.\d$/).test(Utils.release)) {
-    return reject(`Invalid release number passed (${Utils.release}), expect a full semver in the format: MAJOR.MINOR.PATCH`);
-  }
-  Utils.logWelcome();
+if(!RegExp(/\d\.\d\.\d$/).test(Utils.release)) {
+  return Utils.exit(`Invalid release number passed (${Utils.release}), expect a full semver in the format: MAJOR.MINOR.PATCH`);
+}
+Utils.logWelcome();
 
-  github().then(data => {
-    changelog(data, DIR).then(() => {
-      package(data, DIR).then(() => {
-        Utils.exit(null, `Preparations for ${Utils.release} made successfully!`);
-      }).catch(Utils.exit);
+github().then(data => {
+  changelog(data, DIR).then(() => {
+    package(data, DIR).then(() => {
+      Utils.exit(null, `Preparations for ${Utils.release} made successfully!`);
     }).catch(Utils.exit);
   }).catch(Utils.exit);
-})();
+}).catch(Utils.exit);
