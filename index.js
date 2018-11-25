4e20b8a5372675cb7339413623d52ed3d12c6877
Thomas Taylor <thomas.taylor@kineo.com>
2018-11-25 18:20:08 +0000

    Add validation

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const github = require('./lib/github');
 const changelog = require('./lib/changelog');
 const package = require('./lib/package');
 
-if(!RegExp(/\d\.\d\.\d$/).test(Utils.release)) {
-  return Utils.exit(`Invalid release number passed (${Utils.release}), expect a full semver in the format: MAJOR.MINOR.PATCH`);
-}
+Utils.validateRelease();
 Utils.logWelcome();
 
 github().then(data => {
