7bb12028200981d1b2743002dabe5a4d878f3bea
Thomas Taylor <thomas.taylor@kineo.com>
2018-11-25 18:19:59 +0000

    Remove dir param

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const github = require('./lib/github');
 const changelog = require('./lib/changelog');
 const package = require('./lib/package');
 
-const DIR = process.cwd();
-
 if(!RegExp(/\d\.\d\.\d$/).test(Utils.release)) {
   return Utils.exit(`Invalid release number passed (${Utils.release}), expect a full semver in the format: MAJOR.MINOR.PATCH`);
 }
 Utils.logWelcome();
 
 github().then(data => {
-  changelog(data, DIR).then(() => {
-    package(data, DIR).then(() => {
-      Utils.exit(null, `Preparations for ${Utils.release} made successfully!`);
-    }).catch(Utils.exit);
-  }).catch(Utils.exit);
+  Promise.all([
+    changelog(data),
+    package(data)
+  ]).then(() => Utils.exit(null, `Preparations for ${Utils.release} made successfully!`), Utils.exit);
 }).catch(Utils.exit);
